7a643da419f8cf60bf1f85f4ceef696a77ab9bc4
shig07770 <shig07770+dev@gmail.com>
2023-12-23 17:18:05 +0400

    fix(debian/bookworm): relative path issues

--- debian/bookworm/presets/dotfile.sh
+++ debian/bookworm/presets/dotfile.sh
@@ -1,22 +1,24 @@
 # debian/bookworm/presets/dotfile.sh - Dotfiles installation for Debian 12 (Bookworm)
 
+cd "$(dirname $0)/../../.."
+
 rm "$HOME/.profile"
 rm "$HOME/.bash_aliases"
 rm "$HOME/.bash_logout"
 rm "$HOME/.bash_profile"
 rm "$HOME/.bashrc"
 
-cp --recursive ../../dotfile/.cfg ~/.cfg
+cp --recursive dotfile/.cfg ~/.cfg
 
-cp ../../dotfile/.bashrc ~/.bashrc
-cp ../../dotfile/.bash_logout ~/.bash_logout
-cp ../../dotfile/.bash_aliases ~/.bash_aliases
-cp ../../dotfile/.bash_profile ~/.bash_profile
+cp dotfile/.bashrc ~/.bashrc
+cp dotfile/.bash_logout ~/.bash_logout
+cp dotfile/.bash_aliases ~/.bash_aliases
+cp dotfile/.bash_profile ~/.bash_profile
 
-cp ../../dotfile/.vimrc ~/.vimrc
+cp dotfile/.vimrc ~/.vimrc
 
-cp ../../dotfile/.gitconfig ~/.gitconfig
+cp dotfile/.gitconfig ~/.gitconfig
 
 mkdir -p "$HOME/.config/Code/User"
-cp ../../dotfile/.vscode.json ~/.vscode.json
+cp dotfile/.vscode.json ~/.vscode.json
 ln -s "$HOME/.vscode.json" "$HOME/.config/Code/User/settings.json" --force
