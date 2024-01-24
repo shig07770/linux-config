9b926cd24af7c5f9eb7011ca94dffa66f35d578f
shigu07770 <shig07770+git@gmail.com>
2024-01-25 01:08:48 +0400

    add kitty

--- debian/bookworm.sh
+++ debian/bookworm.sh
@@ -58,6 +58,8 @@ minimal() {
   inst_unzip
   inst_openssh
 
+  inst_zsh
+  inst_kitty
   inst_rclone
 
   inst_vlc
@@ -196,7 +198,6 @@ inst_wget() {
 inst_grep() {
   # Installs grep (print lines that match patterns).
   sudo apt install grep
-
 }
 
 inst_curl() {
@@ -228,6 +229,17 @@ inst_chromium() {
 inst_keepassxc() {
   # Installs KeepassXC (a modern open-source password manager).
   sudo apt install keepassxc
+}
+
+inst_kitty() {
+  # Installs Kitty (a fast terminal emulator).
+  # Depends on curl.
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+  ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+  cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+  cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+  sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+  sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 }
 
 inst_rclone() {
